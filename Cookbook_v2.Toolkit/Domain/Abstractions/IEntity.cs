a462e4a4a9614548c05b9aa7418e4bd878f9caa5
Ruprecht <zombiegooood@gmail.com>
2022-08-15 15:40:17 +0300

    Создан интерфейс для всех сущностей

---
+++ Cookbook_v2.Toolkit/Domain/Abstractions/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook_v2.Toolkit.Domain.Abstractions
+{
+    internal interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
