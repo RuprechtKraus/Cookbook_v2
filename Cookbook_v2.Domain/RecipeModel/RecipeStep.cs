4e9743d2a010f03d18b06943ea2860db705ecfac
Ruprecht <zombiegooood@gmail.com>
2022-08-16 14:16:54 +0300

    Добавлены свойства навигации на главную сущность

--- Cookbook_v2.Domain/RecipeModel/RecipeStep.cs
+++ Cookbook_v2.Domain/RecipeModel/RecipeStep.cs
@@ -1,12 +1,12 @@
-﻿using Cookbook_v2.Toolkit.Domain.Abstractions;
+using Cookbook_v2.Toolkit.Domain.Abstractions;
 
 namespace Cookbook_v2.Domain.RecipeModel
 {
     public class RecipeStep : Entity
     {
+        public Recipe Recipe { get; set; }
+        public int RecipeId { get; set; }
         public int Index { get; set; }
         public string Description { get; set; }
-
-        public int RecipeId { get; set; }
     }
 }
