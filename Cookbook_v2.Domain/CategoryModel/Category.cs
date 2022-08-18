8592d0bb3c6a4e968fb3b4db8bc103f75a9b6d93
Ruprecht <zombiegooood@gmail.com>
2022-08-18 18:20:07 +0300

    Добавлена конфигурация для Category

--- Cookbook_v2.Domain/CategoryModel/Category.cs
+++ Cookbook_v2.Domain/CategoryModel/Category.cs
@@ -1,9 +1,11 @@
-﻿using Cookbook_v2.Toolkit.Domain.Abstractions;
+using Cookbook_v2.Toolkit.Domain.Abstractions;
 
 namespace Cookbook_v2.Domain.CategoryModel
 {
     public class Category : Entity
     {
+        public static readonly int s_maxNameLength = 50;
+        public static readonly int s_maxDescriptionLength = 200;
         public string Name { get; set; }
         public string Description { get; set; }
         public string IconUrl { get; set; }
---
+++ Cookbook_v2.Infrastructure/Data/CategoryModel/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Cookbook_v2.Domain.CategoryModel;
+
+namespace Cookbook_v2.Infrastructure.Data.CategoryModel.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure( EntityTypeBuilder<Category> builder )
+        {
+            builder.ToTable("Category");
+            builder.HasKey( x => x.Id );
+
+            builder.Property( x => x.Name )
+                .HasMaxLength( Category.s_maxNameLength )
+                .IsRequired();
+
+            builder.Property( x => x.Description )
+                .HasMaxLength( Category.s_maxDescriptionLength )
+                .IsRequired();
+        }
+    }
+}
