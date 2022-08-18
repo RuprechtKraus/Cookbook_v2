c2629be446103906507947fdd96c67240120752d
Ruprecht <zombiegooood@gmail.com>
2022-08-18 19:17:16 +0300

    Добавлена конфигурация для RecipeStep

--- Cookbook_v2.Domain/RecipeStepModel/RecipeStep.cs
+++ Cookbook_v2.Domain/RecipeStepModel/RecipeStep.cs
@@ -4,6 +4,7 @@ namespace Cookbook_v2.Domain.RecipeStepModel
 {
     public class RecipeStep : Entity
     {
+        public static readonly int s_descriptionMaxLength = 500;
         public int RecipeId { get; set; }
         public int Index { get; set; }
         public string Description { get; set; }
