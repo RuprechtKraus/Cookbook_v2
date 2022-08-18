3d660bb9eb99837bdfbbcac8416a444801d28bbf
Ruprecht <zombiegooood@gmail.com>
2022-08-18 18:00:09 +0300

    Статические поля в User и переименование RecipeFavorite

--- Cookbook_v2.Domain/UserModel/User.cs
+++ Cookbook_v2.Domain/UserModel/User.cs
@@ -6,6 +6,11 @@ namespace Cookbook_v2.Domain.UserModel
 {
     public class User : Entity
     {
+        public static readonly int s_nameMaxLength = 128;
+        public static readonly int s_usernameMaxLength = 128;
+        public static readonly int s_passwordMinLength = 8;
+        public static readonly int s_passwordMaxLength = 32;
+        public static readonly int s_aboutMaxLength = 1000;
         public string Name { get; set; }
         public string Username { get; set; }
         public string About { get; set; }
