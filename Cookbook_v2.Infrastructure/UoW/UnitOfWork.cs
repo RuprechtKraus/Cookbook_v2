3dcf18b34c01d90af8cd6af568192b733140a057
Ruprecht <zombiegooood@gmail.com>
2022-08-19 17:16:44 +0300

    Добавлен Unit Of Work

---
+++ Cookbook_v2.Infrastructure/UoW/UnitOfWork.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using Cookbook_v2.Toolkit.Domain.Abstractions;
+
+namespace Cookbook_v2.Infrastructure.Data.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CookbookContext _context;
+        private bool _disposed = false;
+
+        public UnitOfWork( CookbookContext context )
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            Dispose( true );
+            GC.SuppressFinalize( this );
+        }
+
+        protected virtual void Dispose( bool disposing )
+        {
+            if ( !_disposed )
+            {
+                if ( disposing )
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
