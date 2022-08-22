623f22d17194debd738a32fcc4e880274034caf6
Ruprecht <zombiegooood@gmail.com>
2022-08-22 14:29:22 +0300

    Добавлены биндинги

---
+++ Cookbook_v2.Infrastructure/Data/DataBindings.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using Cookbook_v2.Domain.UserModel;
+using Cookbook_v2.Domain.RecipeModel;
+using Cookbook_v2.Domain.CategoryModel;
+using Cookbook_v2.Infrastructure.Data.UserModel;
+using Cookbook_v2.Infrastructure.Data.RecipeModel;
+using Cookbook_v2.Infrastructure.Data.CategoryModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cookbook_v2.Infrastructure.Data
+{
+    public static class DataBindings
+    {
+        public static IServiceCollection AddRepositories( this IServiceCollection services )
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRecipeRepository, RecipeRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddDatabase<T>( this IServiceCollection services, string connectionString )
+            where T : DbContext
+        {
+            return services
+                .AddDbContext<T>( x => x.UseSqlServer( connectionString ) );
+        }
+    }
+}
