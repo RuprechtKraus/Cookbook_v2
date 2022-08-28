cb6a074067c6ced274365714072f40e4cf7ebe97
Ruprecht <zombiegooood@gmail.com>
2022-08-28 16:22:21 +0300

    Удален MessageContracts

--- Cookbook_v2.Api.MessageContracts/CategoryModel/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Cookbook_v2.Api.MessageContracts.CategoryModel
-{
-    public class CategoryDto
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string IconName { get; set; }
-    }
-}
