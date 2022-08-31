956a09dff5695eae1991bd0c591e8b6b2aeb7dcd
Ruprecht <zombiegooood@gmail.com>
2022-09-01 00:26:05 +0300

    Добавлены конвертеры для коллекций сущностей

--- Cookbook_v2.Application/Helpers/Converters/CollectionConverters.cs
+++ Cookbook_v2.Application/Helpers/Converters/CollectionConverters.cs
@@ -1,8 +1,9 @@
 using Cookbook_v2.Application.Converters;
 using Cookbook_v2.Application.Dtos.RecipeModel;
 using Cookbook_v2.Domain.Entities.RecipeModel;
+using Cookbook_v2.Domain.Entities.TagModel;
 
-namespace Cookbook_v2.Application.Extensions
+namespace Cookbook_v2.Application.Helpers.Converters
 {
     public static class DtoCollectionExtensions
     {
@@ -12,10 +13,21 @@ namespace Cookbook_v2.Application.Extensions
             return dtoCollection.Select( x => x.ToRecipeStep() ).ToList();
         }
 
-        public static List<RecipeIngredientsSection> ToIngredientsSectionList( 
+        public static List<RecipeIngredientsSection> ToIngredientsSectionList(
             this ICollection<RecipeIngredientSectionDto> dtoCollection )
         {
             return dtoCollection.Select( x => x.ToRecipeIngredientsSection() ).ToList();
+        }
+
+        public static List<RecipeStepDto> ToDtoList( this ICollection<RecipeStep> recipeSteps )
+        {
+            return recipeSteps.Select( x => x.ToDto() ).ToList();
+        }
+
+        public static List<RecipeIngredientSectionDto> ToDtoList(
+            this ICollection<RecipeIngredientsSection> recipeIngredients )
+        {
+            return recipeIngredients.Select( x => x.ToDto() ).ToList();
         }
     }
 }
---
+++ Cookbook_v2.Application/Helpers/Converters/TagConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook_v2.Application.Helpers.Converters
+{
+    internal class TagConverter
+    {
+    }
+}
