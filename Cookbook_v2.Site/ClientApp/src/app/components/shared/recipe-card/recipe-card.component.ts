9a25d8eed504e3226291541cc1c8ab16023a6baf
Ruprecht <zombiegooood@gmail.com>
2022-09-10 20:03:13 +0300

    Исправлен редирект с превью рецепта на страницу с его деталями: редирект просходил даже при нажатии на кнопку лайка/добавления в избранное

--- Cookbook_v2.Site/ClientApp/src/app/components/shared/recipe-card/recipe-card.component.ts
+++ Cookbook_v2.Site/ClientApp/src/app/components/shared/recipe-card/recipe-card.component.ts
@@ -9,6 +9,7 @@ import { RecipePreviewDto } from "src/app/dtos/recipe-preview-dto";
 })
 export class RecipeCardComponent implements OnInit {
   @Input() recipe: RecipePreviewDto;
+  @Input() isClickable: boolean;
   onDetailPage: boolean;
 
   constructor(public _router: Router) {}
@@ -17,8 +18,40 @@ export class RecipeCardComponent implements OnInit {
     this.onDetailPage = this.IsOnDetailsPage();
   }
 
+  onClick(event: Event): void {
+    if (this.isClickable) {
+      const target = event.target as any;
+
+      if (!(target.type === "button" || this.IsElementButtonChild(target))) {
+        console.log("Clicked");
+        this._router.navigate([`/recipes/details/${this.recipe.id}`]);
+      }
+    }
+  }
+
+  onLikeClick(): void {
+
+  }
+
+  onFavoriteClick(): void {
+      
+  }
+
   private IsOnDetailsPage(): boolean {
-    var pattern = new RegExp("^\/recipes\/details\/[0-9]+$");
-    return this.onDetailPage = pattern.test(this._router.url);
+    var pattern = new RegExp("^/recipes/details/[0-9]+$");
+    return (this.onDetailPage = pattern.test(this._router.url));
+  }
+  
+  private IsElementButtonChild(element: any): boolean {
+    let parent: any | null = element.parentElement;
+
+    while (parent !== null) {
+      if (parent.type === "button") {
+        return true;
+      }
+      parent = parent.parentElement;
+    }
+    
+    return false;
   }
 }
